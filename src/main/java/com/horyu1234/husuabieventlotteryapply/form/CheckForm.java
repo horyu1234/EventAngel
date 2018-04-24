374c2c52d7c61cd44f44d1ab74f074cd0d137711
horyu1234 <horyu1234@naver.com>
2018-04-24 10:12:00 +0900

    사용되지 않는 변수 제거

--- src/main/java/com/horyu1234/husuabieventlotteryapply/form/CheckForm.java
+++ src/main/java/com/horyu1234/husuabieventlotteryapply/form/CheckForm.java
@@ -9,22 +9,11 @@ public class CheckForm {
     @NotNull
     private String email;
 
-    @NotNull
-    private String recaptchaResponse;
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getRecaptchaResponse() {
-        return recaptchaResponse;
-    }
-
-    public void setRecaptchaResponse(String recaptchaResponse) {
-        this.recaptchaResponse = recaptchaResponse;
     }
 }
