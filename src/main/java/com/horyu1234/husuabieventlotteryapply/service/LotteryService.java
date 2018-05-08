aca72efef12a2daa901d8aeaf3001101f353a976
horyu1234 <horyu1234@naver.com>
2018-05-08 13:42:55 +0900

    이미 추첨된 사람이 추첨에서 제외되지 않도록 수정

--- src/main/java/com/horyu1234/husuabieventlotteryapply/service/LotteryService.java
+++ src/main/java/com/horyu1234/husuabieventlotteryapply/service/LotteryService.java
@@ -3,12 +3,10 @@ package com.horyu1234.husuabieventlotteryapply.service;
 import com.horyu1234.husuabieventlotteryapply.constant.EventStatus;
 import com.horyu1234.husuabieventlotteryapply.domain.Applicant;
 import com.horyu1234.husuabieventlotteryapply.domain.Event;
-import com.horyu1234.husuabieventlotteryapply.domain.EventWinner;
 import org.apache.commons.math3.random.MersenneTwister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,40 +41,16 @@ public class LotteryService {
             eventService.updateEvent(currentEvent);
         }
 
-        int eventId = currentEvent.getEventId();
-
         List<Applicant> applicantList = applicantService.getApplicantList();
-        applicantList = removeLotteryedPeople(applicantList, currentEvent.getEventId());
 
         MersenneTwister mersenneTwister = new MersenneTwister();
         int randomIndex = mersenneTwister.nextInt(applicantList.size());
 
         Applicant lotteryedApplicant = applicantList.get(randomIndex);
+
+        int eventId = currentEvent.getEventId();
         eventWinnerService.addEventWinner(eventId, prizeId, lotteryedApplicant);
 
         return lotteryedApplicant;
-    }
-
-    private List<Applicant> removeLotteryedPeople(List<Applicant> applicantList, int eventId) {
-        List<EventWinner> eventWinnerList = eventWinnerService.getWinnerList(eventId);
-
-        List<Applicant> applicants = new ArrayList<>();
-        for (Applicant applicant : applicantList) {
-            if (!isLotteryed(eventWinnerList, applicant.getEmail())) {
-                applicants.add(applicant);
-            }
-        }
-
-        return applicants;
-    }
-
-    private boolean isLotteryed(List<EventWinner> eventWinnerList, String email) {
-        for (EventWinner eventWinner : eventWinnerList) {
-            if (eventWinner.getApplyEmail().equals(email)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
