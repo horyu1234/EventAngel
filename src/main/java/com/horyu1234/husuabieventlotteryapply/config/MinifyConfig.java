421bcf9c72e8da0b83d14ece3011072b53c0232c
horyu1234 <horyu1234@naver.com>
2018-06-13 01:37:55 +0900

    FilterRegistrationBean 에 타입 지정

--- src/main/java/com/horyu1234/husuabieventlotteryapply/config/MinifyConfig.java
+++ src/main/java/com/horyu1234/husuabieventlotteryapply/config/MinifyConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MinifyConfig {
     @Bean
-    public FilterRegistrationBean getFilterRegistrationBean() {
-        return new FilterRegistrationBean(new MinifyFilter());
+    public FilterRegistrationBean<MinifyFilter> getFilterRegistrationBean() {
+        return new FilterRegistrationBean<>(new MinifyFilter());
     }
 }
