7fc4eca798deaeef4a147de7a5ae15d480c11946
horyu1234 <horyu1234@naver.com>
2018-07-03 00:16:45 +0900

    중복 응모자 확인 메뉴에서 통신사 아이피는 비고란에 정보가 나오도록 수정

--- src/main/resources/static/js/admin/Horyu.View.Duplicated.js
+++ src/main/resources/static/js/admin/Horyu.View.Duplicated.js
@@ -63,8 +63,50 @@ Horyu.View.Duplicated = function() {
             $tr.append($('<td>').text(data.columnName));
             $tr.append($('<td class="duplicated-count">').text(data.duplicatedCount + '개'));
 
+            if (_this.getCurrentColumnId() === 'IP_ADDRESS') {
+                $tr.append($('<td class="remarks">').text(_this.getCarrierInformation(data.columnName)));
+            } else {
+                $tr.append($('<td class="remarks">').text(''));
+            }
+
             $tableBody.append($tr);
         });
+    };
+
+    _this.getCarrierInformation = function(ip) {
+        var SKT_3G = ['203.226', '211.234'];
+        var SKT_LTE = ['115.161', '121.190', '122.202', '122.32', '175.202', '223.32', '223.33', '223.62', '223.38', '223.39', '223.57'];
+        var KT_3G = ['39.7', '110.70', '175.223', '175.252', '211.246'];
+        var KT_LTE = ['39.7', '110.70', '175.223', '175.252', '210.125', '211.246'];
+        var LG_UPLUS_3G = ['61.43', '211.234'];
+        var LG_UPLUS_LTE = ['114.200', '117.111', '211.36', '106.102', '125.188'];
+
+        if (_this.containsIpRange(SKT_3G, ip)) {
+            return '통신사 아이피 (SKT 3G)';
+        } else if (_this.containsIpRange(SKT_LTE, ip)) {
+            return '통신사 아이피 (SKT LTE)';
+        } else if (_this.containsIpRange(KT_3G, ip)) {
+            return '통신사 아이피 (KT 3G/LTE)';
+        } else if (_this.containsIpRange(KT_LTE, ip)) {
+            return '통신사 아이피 (LTE)';
+        } else if (_this.containsIpRange(LG_UPLUS_3G, ip)) {
+            return '통신사 아이피 (LG U+ 3G)';
+        } else if (_this.containsIpRange(LG_UPLUS_LTE, ip)) {
+            return '통신사 아이피 (LG U+ LTE)';
+        } else {
+            return '';
+        }
+    };
+
+    _this.containsIpRange = function(ipRange, searchIp) {
+        var hasIpRange = false;
+        ipRange.forEach(function(ip) {
+            if (searchIp.startsWith(ip)) {
+                hasIpRange = true;
+            }
+        });
+
+        return hasIpRange;
     };
 
     _this.getCurrentColumnId = function() {
