dcc0c54168546f4332dd5e6a5360ab0eab875aa6
horyu1234 <horyu1234@naver.com>
2018-07-05 18:11:25 +0900

    이벤트 기록 메뉴에서 당시 응모자 목록도 확인 가능하도록 수정

--- src/main/java/com/horyu1234/eventangel/controller/admin/EventHistoryController.java
+++ src/main/java/com/horyu1234/eventangel/controller/admin/EventHistoryController.java
@@ -1,7 +1,9 @@
 package com.horyu1234.eventangel.controller.admin;
 
 import com.horyu1234.eventangel.constant.View;
+import com.horyu1234.eventangel.domain.Event;
 import com.horyu1234.eventangel.factory.ModelAttributeNameFactory;
+import com.horyu1234.eventangel.service.ApplicantService;
 import com.horyu1234.eventangel.service.EventService;
 import com.horyu1234.eventangel.service.EventWinnerService;
 import com.horyu1234.eventangel.service.PrizeService;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by horyu on 2018-04-16
  */
@@ -21,17 +27,31 @@ public class EventHistoryController {
     private EventService eventService;
     private PrizeService prizeService;
     private EventWinnerService eventWinnerService;
+    private ApplicantService applicantService;
 
     @Autowired
-    public EventHistoryController(EventService eventService, PrizeService prizeService, EventWinnerService eventWinnerService) {
+    public EventHistoryController(EventService eventService, PrizeService prizeService, EventWinnerService eventWinnerService, ApplicantService applicantService) {
         this.eventService = eventService;
         this.prizeService = prizeService;
         this.eventWinnerService = eventWinnerService;
+        this.applicantService = applicantService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String eventHistory(Model model) {
-        model.addAttribute("eventList", eventService.getEventList());
+        List<Event> eventList = eventService.getEventList();
+        Map<Integer, Long> applicantCountMap = new HashMap<>();
+
+        for (Event event : eventList) {
+            int eventId = event.getEventId();
+
+            long applyCount = applicantService.getApplyCount(eventId, true);
+            applicantCountMap.put(eventId, applyCount);
+        }
+
+        model.addAttribute("eventList", eventList);
+        model.addAttribute("applicantCountMap", applicantCountMap);
+
         model.addAttribute(ModelAttributeNameFactory.VIEW_NAME, View.ADMIN_EVENT_HISTORY.toView());
 
         return View.LAYOUT.getTemplateName();
--- src/main/resources/static/js/admin/Horyu.View.EventHistory.js
+++ src/main/resources/static/js/admin/Horyu.View.EventHistory.js
@@ -2,11 +2,70 @@
 
 var Horyu = window.Horyu || {};
 Horyu.View = window.Horyu.View || {};
-Horyu.View.EventHistory = function() {
+Horyu.View.EventHistory = function(options) {
     var _this = this;
 
+    var defaultOptions = {
+        eventList: [],
+        applicantCountMap: {}
+    };
+
+    _this.options = $.extend(defaultOptions, options);
+
     _this.init = function() {
+        _this.changeMainContainerWidth();
+        _this.updateTable();
         _this.bindEvents();
+    };
+
+    _this.changeMainContainerWidth = function() {
+        var $mainContainer = $('#main-container');
+        $mainContainer.removeClass('container');
+        $mainContainer.addClass('container-full');
+    };
+
+    _this.updateTable = function() {
+        var $table = $('.event-history-table');
+
+        var $tableBody = $table.find('tbody');
+        $tableBody.empty();
+
+        _this.options.eventList.forEach(function(event) {
+            var $tr = $('<tr></tr>');
+            $tr.append($('<td>').text(event.eventId));
+            $tr.append($('<td>').text(event.eventTitle));
+            $tr.append($('<td>').text(event.eventDetail));
+            $tr.append($('<td>').text(_this.getEventStatusText(event.eventStatus)));
+            $tr.append($('<td>').html(moment(event.eventStartTime).format('YYYY년 MMMM D일 dddd<br/>A h시 mm분 ss초')));
+            $tr.append($('<td>').html(moment(event.eventEndTime).format('YYYY년 MMMM D일 dddd<br/>A h시 mm분 ss초')));
+
+            var applicantCount = _this.options.applicantCountMap[event.eventId];
+            if (event.eventId > 5) {
+                $tr.append($('<td>').text(applicantCount + '개'));
+            } else {
+                $tr.append($('<td>').text('-'));
+            }
+
+            var $eventDetailBtn = $('<button type="button" class="btn btn-success btn-sm viewEventBtn"></button>')
+                .text('이벤트 결과 확인')
+                .attr('data-event-id', event.eventId);
+
+            $tr.append($('<td>').append($eventDetailBtn));
+
+            $tableBody.append($tr);
+        });
+    };
+
+    _this.getEventStatusText = function(eventStatus) {
+        if (eventStatus === 'OPEN') {
+            return '응모 중';
+        } else if (eventStatus === 'CLOSE') {
+            return '응모 닫힘';
+        } else if (eventStatus === 'LOTTERY') {
+            return '추첨 진행';
+        } else {
+            return eventStatus;
+        }
     };
 
     _this.bindEvents = function() {
