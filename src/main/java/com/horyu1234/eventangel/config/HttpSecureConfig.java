44da113120fa2e19db46027cafcea670a6d4cc01
horyu1234 <horyu1234@naver.com>
2018-07-30 15:00:31 +0900

    어플리케이션 자체적으로 HTTP 로 접속 시 자동으로 HTTPS 로 이동시키도록 수정

---
+++ src/main/java/com/horyu1234/eventangel/config/HttpSecureConfig.java
@@ -0,0 +1,48 @@
+package com.horyu1234.eventangel.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpSecureConfig {
+    @Value("${server.port}")
+    private int httpSecurePort;
+
+    @Value("${server.http-port}")
+    private int httpPort;
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpSecurePort);
+
+        return connector;
+    }
+}
